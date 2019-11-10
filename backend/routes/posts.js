b6698ed41b6fed76906ee4cbf294e92a5e722e1b
polgasull <pgasulln@gmail.com>
2019-11-10 23:23:44 +0100

    Create Post with image

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,6 +1,29 @@
 const express = require('express');
+const multer = require('multer');
 const router = express.Router();
-const Post = require('../models/post')
+const Post = require('../models/post');
+
+const MIME_TYPE_MAP = {
+  'image/png': 'png',
+  'image/jpeg': 'jpg',
+  'image/jpg': 'jpg'
+}
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const isValid = MIME_TYPE_MAP[file.mimetype];
+    let error = new Error("Invalid myme type");
+    if (isValid) {
+      error = null;
+    }
+    cb(error, "backend/images")
+  },
+  filename: (req, file, cb) => {
+    const name = file.originalname.toLowerCase().split(' ').join('-');
+    const extension = MIME_TYPE_MAP[file.mimetype];
+    cb(null, name + '-' + Date.now() + '.' + extension)
+  }
+});
 
 router.get("", (req, res, next) => {
   Post.find().then(documents => {
@@ -21,10 +44,12 @@ router.get("/:id", (req, res, next) => {
   })
 })
 
-router.post("", (req, res, next) => {
+router.post("", multer({storage: storage}).single("image"), (req, res, next) => {
+  const url = req.protocol + '://' + req.get("host");
   const post = new Post({
     title: req.body.title,
-    content: req.body.content
+    content: req.body.content,
+    imagePath: url + "/images/" + req.file.filename
   });
   post.save().then(createdPost => {
     res.status(201).json({
