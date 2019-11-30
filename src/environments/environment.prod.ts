4337ce4da2110fff4cc8feb30a2d9d120ea2ec8c
polgasull <pgasulln@gmail.com>
2019-11-30 11:35:04 +0100

    changed title tag

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  // apiURL: 'http://getawaysmeanstack-env.kyqn2pf4vd.eu-west-1.elasticbeanstalk.com/api'
+  apiURL: 'http://getawaysmeanstack-env.kyqn2pf4vd.eu-west-1.elasticbeanstalk.com/api'
 };
