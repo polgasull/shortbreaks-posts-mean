7d14316015790b1cb182dc2c7c78f372ee23ab1d
polgasull <pgasulln@gmail.com>
2019-12-01 11:39:15 +0100

    Changed images path

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,7 @@ mongoose.connect(
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/images", express.static(path.join("./backend/images")));
+app.use("/images", express.static(path.join("images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
